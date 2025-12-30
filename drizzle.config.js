b3aaee9478d4b35b373579dfdbc477add0eb8186
chakriswamireddy <iamthechakri@gmail.com>
2025-12-31 01:21:35 +0530

    ref - drizzle added env

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,4 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 module.exports = {
     schema: "./src/models/binSchema.js",
     out: "./drizzle",
